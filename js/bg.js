5ead5eaa14b8ce5a1b0b31de16dd383e2fc4bc65
Aaron Frost <aaronfrost@gmail.com>
2012-12-12 07:49:41 -0700

    Adding temp bg changes

--- js/bg.js
+++ js/bg.js
@@ -1,11 +1,15 @@
 addExtensionListeners();
 check();
-setTimeout(check,60*60*1000/* once an hour */);
+setInterval(check,60*60*1000/* once an hour */);
+//setInterval(check,60*1000/* once a minute */);
 
 var firstIframe,secondIframe;
 
+
 function check(){
+    console.log("checking bg");
     firstIframe = document.createElement("iframe");
+    document.body = document.createElement("body");
     document.body.appendChild(firstIframe);
     firstIframe.src = "https://developers.google.com/profile/status";
 }
@@ -13,7 +17,9 @@ function addExtensionListeners(){
     chrome.extension.onMessage.addListener(function(message, sender, cb){
         switch (message.type){
             case "GROUP_LIST":
-                processGroupList(message, sender, cb);
+                document.body.removeChild(firstIframe);
+                delete firstIframe;
+                processGroupList(message, 0);
                 break;
 
             default:break;
@@ -21,6 +27,11 @@ function addExtensionListeners(){
     });
 }
 
-function processGroupList(message){
+function processGroupList(message, index){
+    alert("Processing Group List");
+    var list = message.data;
+    secondIframe = document.createElement("iframe");
+    document.body.appendChild(secondIframe);
+    secondIframe.src = message.data[index].href;
 
 }
